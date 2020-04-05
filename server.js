b58e7244261c8a5fb330e6cf3ff140b1878414ce
vonBrax <alexandrevbf84@gmail.com>
2020-04-05 14:30:53 +0200

    feat(worker): add temporary routes to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // eslint-disable
-
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 // const path = require('path');
@@ -16,6 +16,19 @@ app.get('/shaders', (req, res) => {
   const list = fs.readdirSync('static/shaders', 'utf-8');
   res.status(200).send({ status: 200, data: list });
   res.end();
+});
+
+app.get('/a.out.worker.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/liquidfun/a.out.worker.js'));
+});
+app.get('/a.out.wasm', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/liquidfun/a.out.wasm'));
+});
+app.get('/liquidfun.worker.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/liquidfun/liquidfun.worker.js'));
+});
+app.get('/liquidfun.wasm', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/liquidfun/liquidfun.wasm'));
 });
 
 app.listen(5555, () => {
