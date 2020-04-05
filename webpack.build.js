f8c438a3b59d76d1c2ac24fd39bb627fafe3f774
vonBrax <alexandrevbf84@gmail.com>
2020-04-05 14:29:51 +0200

    build(webpack): add build command

---
+++ webpack.build.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.ts',
+    worker: './src/worker/worker.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['main'],
+      template: 'index.html',
+    }),
+  ],
+};
