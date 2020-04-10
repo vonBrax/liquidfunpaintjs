d39e43c611a5dfbdabfdbfa26daed9ee9eb9b6a7
vonBrax <alexandrevbf84@gmail.com>
2020-04-10 11:46:25 +0200

    build: add build step for liquidfun

--- build/em-ubuntu/entry.sh
+++ build/em-ubuntu/entry.sh
@@ -4,11 +4,11 @@
 
 set -eu
 
-TAG=1.39.4
+TAG=1.39.3
 UPDATE="update"
 SHELL="shell"
 
-emsdk_portable/entrypoint
+# /emsdk_portable/entrypoint
 
 if [ "$#" -ne 1 ]
 then
@@ -26,10 +26,16 @@ then
   cd /emsdk_portable/emscripten/tag-$TAG
   git pull
   echo "Pulling liquidun"
-  cd /liquidfun
-  git checkout feature/embind
-  git pull
-  cd liquidfun/Box2D/lfjs
+  git clone -b feature/embind --depth 1 https://github.com/vonBrax/liquidfun.git /tmp/lf && \
+  mkdir -p /liquidfun/liquidfun/Box2D/Box2D && \
+  mkdir -p /liquidfun/liquidfun/Box2D/lfjs && \
+  cp -r /tmp/lf/liquidfun/Box2D/Box2D /liquidfun/liquidfun/Box2D && \
+  cp -r /tmp/lf/liquidfun/Box2D/lfjs /liquidfun/liquidfun/Box2D && \
+  rm -rf /tmp/lf \
+  # cd /liquidfun
+  #git checkout feature/embind
+  #git pull
+  cd /liquidfun/liquidfun/Box2D/lfjs
   echo "Compiling liquidfun"
   emcc -I ../ -o /src/liquidfun.js --bind ./jsBindings/bindings.cpp -Oz --proxy-to-worker -s "EXTRA_EXPORTED_RUNTIME_METHODS=['getValue']" -s "ENVIRONMENT=web,worker"
 elif [[ "$1" == "$SHELL" ]]
